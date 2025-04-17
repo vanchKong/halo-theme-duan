40cb1fd6085608e6ca0e413d31235b4f246a44f6
vanch <vanchkong@qq.co>
2025-04-17 18:02:32 +0800

    更新Tailwind配置，添加响应式屏幕尺寸；修改CSS以支持新布局和样式；优化HTML模板结构，增强可读性和可维护性。

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,19 @@
 /** @type {import('tailwindcss').Config} */
-const { addDynamicIconSelectors } = require('@iconify/tailwind');
+const { addDynamicIconSelectors } = require("@iconify/tailwind");
 module.exports = {
   content: ["./templates/**/*.html", "./src/main.ts"],
   theme: {
     extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      cs: "1183px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
   },
-  plugins: [
-    require("@tailwindcss/typography"),
-    addDynamicIconSelectors(),
-  ],
+  plugins: [require("@tailwindcss/typography"), addDynamicIconSelectors()],
   safelist: [
     "prose-sm",
     "prose-base",
